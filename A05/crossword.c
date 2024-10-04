9b8121597c7826c117c8761e1072361267f88d13
Grace Elizabeth Swenson Hollis <gswensonhollis@goldengate.cs.brynmawr.edu>
2024-10-04 13:30:13 -0400

    assignment complete

--- A05/crossword.c
+++ A05/crossword.c
@@ -1,10 +1,131 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Grace Swenson Hollis
+ * Date: October 10,2024
+ * Description Crossword puzzle 
  ---------------------------------------------*/
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
 
-int main() {
+int sizeMe(char *str){
+  int count = 0;
+  while (str[count]!='\0')
+  {
+    count++;
+  }
+  
+  return count;
+}
+
+void wordTime(int sa,int sb,int spotA,int spotB,char *wA,char *wB){
+
+  //https://www.geeksforgeeks.org/dynamically-allocate-2d-array-c/ helped out with 2d array
+
+
+    //make it
+    char** crossword = (char**)malloc(sa * sizeof(char*));
+    for (int i = 0; i < sa; i++)
+    {
+        crossword[i] = (char*)malloc(sb * sizeof(char));
+    }
+
+    //fill it
+    int countA = 0;
+    int countB = 0;
+    for (int i = 0; i < sa; i++)
+    {
+      for (int j = 0; j < sb; j++)
+      {
+        if(i==spotA&&j==spotB)
+        {
+          crossword[i][j] = wB[countB]; 
+          countA++;
+          countB++;
+
+        }
+        else if(i==spotA)
+        {
+          crossword[i][j] = wB[countB]; 
+          countB++;
+        }
+        else if(j==spotB)
+        {
+          
+          crossword[i][j] = wA[countA]; 
+          countA++;
+        }
+        else
+        {
+        crossword[i][j] = '.';
+        }
+      }
+    }
+    //print it
+    for (int i = 0; i < sa; i++)
+    {
+        for (int j = 0; j < sb; j++){
+          printf("%c ", crossword[i][j]);
+        }
+        printf("\n");
+    }
+
+//free it
+    for (int i = 0; i < sa; i++)
+    {
+        free(crossword[i]);
+    }
+
+    free(crossword); 
+}
+
+int main(int argc, char *argv[]) {
+   if (argc < 3) {
+    printf("Usage: %s <word1> <word2>\n", argv[0]);
+    exit(0);
+  }
+
+char *wordA=argv[1];
+char *wordB=argv[2];
+
+int sizeA=sizeMe(wordA);
+int sizeB=sizeMe(wordB);
+
+
+char sharedChar='\0';
+int a = -1;
+int b = -1;
+
+for(int i = 0; i < sizeA; i++)
+{
+  for(int j = 0; j < sizeB; j++)
+  {
+    if(sharedChar=='\0')
+    {
+      if(wordA[i]==wordB[j])
+      {
+        sharedChar=wordA[i];
+        a=i;
+        b=j;
+      }
+    }
+  }
+}
+
+if(sharedChar=='\0'){
+  printf("No common letter!");
+  exit(0);
+}
+
+if(sizeA>sizeB)
+{
+  wordTime( sizeA,sizeB,a,b,wordA,wordB);
+
+}
+else
+{
+    wordTime(sizeB,sizeA,b,a,wordB,wordA);
+}
+
+
   return 0;
 }
--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -14,6 +14,94 @@ struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
   return NULL;
 }
 
+
+
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
-  return NULL;
+
+  FILE *infile= fopen(filename, "r");
+  if (infile == NULL) 
+  {
+    return NULL;
+    exit(1);
+  }
+
+  char line1[4];
+  fgets (line1, 4, infile);
+
+
+/*
+  char line[1000];
+  fgets(line, 100, infile);//magic number
+  fgets(line, 100, infile);//stuff
+  fgets(line, 100, infile);//width and height
+
+  int count = 0;
+  char* token = strtok(ppm[2]," ");
+
+   while (token != NULL) {
+       if(count==0){
+        *w = atoi(token);
+       }
+       else if(count==1){
+         *h = atoi(token);
+       }
+        token = strtok(NULL," ");
+        count++;
+    }
+
+   
+ 
+  fgets(line, 100, infile);//stuff
+  */
+
+  char ppm[100];
+  fgets (ppm, 100, infile); 
+  int max;
+  fscanf (infile, "%d %d\n%d\n", w, h, &max);
+
+  struct ppm_pixel **pixels = malloc(sizeof(struct ppm_pixel *) * (*w * *h));
+  if (pixels ==NULL) 
+  {
+    printf("Error! No space \n");
+    return NULL;
+  }
+
+  for (int i = 0; i<*h; i++)
+  {
+    pixels[i] = malloc(sizeof(struct ppm_pixel) * *w);
+  }
+
+  for(int i = 0; i<*h; i++) 
+  {
+    for(int j = 0; j<*w; j++)
+    {
+      unsigned char ch = getc(infile);
+      struct ppm_pixel Pix;
+      Pix.red = ch;
+
+      ch = fgetc(infile);
+      Pix.green = ch;
+
+      ch = fgetc(infile);
+
+      Pix.blue = ch;
+
+      pixels[i][j] = Pix;
+
+    }
+    
+
+  }
+
+  printf("Testing file %s: %d %d\n",filename,*w,*h);
+
+
+
+
+
+
+
+
+  fclose(infile);
+  return pixels;
 }
