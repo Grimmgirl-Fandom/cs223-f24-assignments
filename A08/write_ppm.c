36e6c1941cd735656eaa35a459f609b1049f3488
Grace Elizabeth Swenson Hollis <gswensonhollis@goldengate.cs.brynmawr.edu>
2024-11-01 15:07:59 -0400

    assignment complete

--- A08/write_ppm.c
+++ A08/write_ppm.c
@@ -1,9 +1,44 @@
+/*----------------------------------------------
+ * Author: 
+ * Date: 
+ * Description
+ ---------------------------------------------*/
 #include "write_ppm.h"
 #include <stdio.h>
 #include <string.h>
 
+// implement *one* (but do not remove the other one)!
+
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+
 }
 
-void write_ppm_2d(const char* filename, struct ppm_pixel** pxs, int w, int h) {
+void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
+    FILE *fptr;
+    fptr = fopen(filename, "wb");
+
+    if (fptr == NULL) 
+    {
+        printf("Error. File failed to open");
+    }
+    else
+    {
+        fprintf(fptr, "P6\n");
+        fprintf(fptr,"# Created by GIMP version 2.10.24 PNM plug-in\n");
+        fprintf(fptr, "4 4\n");
+        fprintf(fptr, "255\n");
+
+
+       // *prt;
+
+        for(int i = 0; i<h; i++) 
+            { 
+            fwrite(pixels[i], sizeof(struct ppm_pixel), w, fptr);
+            }
+
+    }
+
+    fclose(fptr);
+
+
 }
