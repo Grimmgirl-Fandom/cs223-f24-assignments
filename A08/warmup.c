d395dac328f407cf8c90150c10c63c5d19f252da
Grace Swenson Hollis <130778996+Grimmgirl-Fandom@users.noreply.github.com>
2024-11-06 22:30:47 -0500

    Update warmup.c

--- A08/warmup.c
+++ A08/warmup.c
@@ -1,5 +1,51 @@
-#include <stdio.h>
+//Grace Swenson Hollis
+//October 2024
 
-int main() {
-  return 0;
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main()
+{
+
+  //try with wait
+  char c = 'A';
+  
+  int n = 0;
+
+  printf("%d] %c%d\n",getpid(),c,n);//A0
+
+  pid_t p;
+  p=fork();
+
+
+   if(p==0)
+  {
+    c++;
+    n++;
+    printf("%d] %c%d\n",getpid(),c,n);//B1
+    printf("%d] Bye\n",getpid());
+    c++;
+    p=fork();
+    if(p==0)
+    {
+      printf("%d] %c%d\n",getpid(),c,n);//C1
+      printf("%d] Bye\n",getpid());
+    }
+  }
+    else
+    {
+      c++;
+      printf("%d] %c%d\n",getpid(),c,n);//B0
+      p=fork();
+      c++;
+      if(p==0)
+      {
+        printf("%d] %c%d\n",getpid(),c,n);//C1
+        printf("%d] Bye\n",getpid());
+        }
+    }
+  
+      
+
+    return 0;
 }
