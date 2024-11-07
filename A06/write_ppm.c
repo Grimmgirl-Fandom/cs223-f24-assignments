1c7641ad1781898588ad6c3e52805b56b4cf457c
Grace Swenson Hollis <130778996+Grimmgirl-Fandom@users.noreply.github.com>
2024-11-06 22:29:27 -0500

    Update write_ppm.c

--- A06/write_ppm.c
+++ A06/write_ppm.c
@@ -14,5 +14,31 @@ void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
 }
 
 void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
+    FILE *fptr;
+    fptr = fopen(filename, "wb");
+
+    if (fptr == NULL) 
+    {
+        printf("Error. File failed to open");
+    }
+    else
+    {
+        fprintf(fptr, "P6\n");
+        fprintf(fptr,"# Created by GIMP version 2.10.24 PNM plug-in\n");
+        fprintf(fptr, "4 4\n");
+        fprintf(fptr, "255\n");
+
+
+       // *prt;
+
+        for(int i = 0; i<h; i++) 
+            { 
+            fwrite(pixels[i], sizeof(struct ppm_pixel), w, fptr);
+            }
+
+    }
+
+    fclose(fptr);
+
 
 }
