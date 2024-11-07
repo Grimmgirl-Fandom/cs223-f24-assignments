20edb5811ce006581262274f385fa800f404acaf
Grace Swenson Hollis <130778996+Grimmgirl-Fandom@users.noreply.github.com>
2024-11-06 22:28:44 -0500

    Update encode.c

--- A06/encode.c
+++ A06/encode.c
@@ -15,6 +15,66 @@ int main(int argc, char** argv) {
     return 0;
   }
 
+  int w, h;
+  char *buff= argv[1];
+
+  struct ppm_pixel** pixels = read_ppm_2d(buff, &w, &h);
+
+ FILE *infile= fopen(buff, "r");
+  if (infile == NULL) 
+  {
+    exit(1);
+  }
+
+
+//buffer
+  int str[100];
+  int chars = 0;
+  int index = 0;
+
+
+  for(int i = 0; i<h; i++) 
+  {
+
+    for(int j = 0; j<w; j++)
+    { 
+     // printf("(%d,%d,%d) ",pixels[i][j].red,pixels[i][j].green,pixels[i][j].blue);
+      unsigned int r =pixels[i][j].red;
+      str[index]=r;
+      index++;
+      unsigned int g = pixels[i][j].green;
+      str[index]=g;
+      index++;
+      unsigned int b = pixels[i][j].blue;
+      str[index]=b;
+      index++;
+     // printf("%d%d%d", r,g,b);
+      //printf("%02X\n", r);
+    }
+    //printf("\n");
+  }
+  str[index+1]='\0';
+
+  chars=(index-1)/8;
+
+  printf("Reading %s with width %d and height %d\n",buff,w,h);
+  printf("Max number of characters in the image: %d\n",chars);
+  printf("Enter a phrase: ");
+  char val[100];
+  scanf("%s",val);
+
+
+   // test writing the file to test.ppm
+  write_ppm_2d("feep-raw-encoded.ppm",pixels,w,h);
+
+   for(int i = 0; i<h; i++) 
+  {
+    free(pixels[i]);
+
+  }
+
+  free(pixels); 
+
   return 0;
 }
 
