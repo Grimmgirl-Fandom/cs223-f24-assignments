541f65035ab02b88a596757dc49c54cef8f18fc1
Grace Swenson Hollis <130778996+Grimmgirl-Fandom@users.noreply.github.com>
2024-11-06 22:25:52 -0500

    Update test_read.c

--- A05/test_read.c
+++ A05/test_read.c
@@ -9,9 +9,29 @@
 
 int main() {
   int w, h;
-  struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
+  struct ppm_pixel** pixels = read_ppm_2d("feep-raw.ppm", &w, &h);
 
   // todo: print out the grid of pixels
+
+
+
+
+  for(int i = 0; i<h; i++) 
+  {
+
+    for(int j = 0; j<w; j++)
+    { 
+      printf("(%d,%d,%d) ",pixels[i][j].red,pixels[i][j].green,pixels[i][j].blue);
+    }
+    printf("\n");
+  }
+
+
+  //free
+   for(int i = 0; i<h; i++) 
+  {
+    free(pixels[i]);
+  }
   free(pixels);
   return 0;
 }
