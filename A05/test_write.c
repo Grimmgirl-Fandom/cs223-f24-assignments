14974b00d07c84020724e46e1ebda4c56e0ae994
Grace Swenson Hollis <130778996+Grimmgirl-Fandom@users.noreply.github.com>
2024-11-06 22:26:06 -0500

    Update test_write.c

--- A05/test_write.c
+++ A05/test_write.c
@@ -12,9 +12,52 @@
 
 int main(int argc, char** argv) {
   int w, h;
-  struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
+  struct ppm_pixel** pixels = read_ppm_2d("feep-raw.ppm", &w, &h);
 
-  // test writing the file to test.ppm, reload it, and print the contents
-  free(pixels);
+ FILE *infile= fopen("test.ppm", "r");
+  if (infile == NULL) 
+  {
+    exit(1);
+  }
+
+  // test writing the file to test.ppm
+  write_ppm_2d("test.ppm",pixels,w,h);
+
+   for(int i = 0; i<h; i++) 
+  {
+    free(pixels[i]);
+
+  }
+
+  free(pixels); 
+
+//reload it
+  struct ppm_pixel** pixels2 = read_ppm_2d("test.ppm", &w, &h);
+
+// print the contents
+
+  for(int i = 0; i<h; i++) 
+  {
+
+    for(int j = 0; j<w; j++)
+    { 
+      printf("(%d,%d,%d) ",pixels2[i][j].red,pixels2[i][j].green,pixels2[i][j].blue);
+    }
+    printf("\n");
+  }
+
+
+  //free
+
+
+     for(int i = 0; i<h; i++) 
+  {
+    free(pixels2[i]);
+
+  }
+
+  free(pixels2); 
+ fclose(infile);
+
   return 0;
 }
