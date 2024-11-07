9d367c3f6d1b3958f94a84047302aa9df6832ad2
Grace Swenson Hollis <130778996+Grimmgirl-Fandom@users.noreply.github.com>
2024-11-06 22:19:24 -0500

    Update acronym.c

--- A02/acronym.c
+++ A02/acronym.c
@@ -1,5 +1,52 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+
+//note only counts if each word starts with a capital letter and no random letters are capital
+
+int wordCount(char stuff[]){
+  int count = 0;
+  int len = 0;
+  
+  while(stuff[len]!='\0')
+  {
+    if((int)stuff[len]>64&&(int)stuff[len]<91)
+    {
+      count++;
+    }
+    len++;
+  }
+
+  return count;
+}
+
+
 
 int main() {
+
+
+  printf("\nEnter a phrase: "); 
+  char phrase[1024];
+  scanf("%[^\n]%*c", phrase);
+
+  int siz= wordCount(phrase);
+  char acr[1024];
+  int spot = 0;
+  //fill it
+  int i = 0;
+  acr[siz]='\0';
+  while(spot<siz){
+    if((int)phrase[i]>64&&(int)phrase[i]<91){
+      acr[spot]=phrase[i];
+      spot++;
+      }
+    i++;
+  }
+
+  printf("\n Your acronym is: "); 
+  printf("%s\n",acr);
+
+
   return 0;
+
 }
